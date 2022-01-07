b6aa0900fe107c40022624a158b0b849d1757b2c
rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-01-07 18:04:37 -0200

    update

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,8 +1,8 @@
 class Produto < ApplicationRecord
     validates :codigo, presence: true, length: { minimum: 3}
-    validates :nome, presence: true, length: { minimum: 5}
+    validates :nome, presence: true, length: { minimum: 20}
     validates :classificacao, presence: true , length: { minimum: 2}
-    validates :descricao, presence: true, length: { minimum: 50}
+    validates :descricao, presence: true, length: { minimum: 10}
 
     
     enum classificacao: { 'Produto' => 0, 'Servico' => 1 }
