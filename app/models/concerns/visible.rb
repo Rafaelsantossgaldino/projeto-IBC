0fc9be2ad9064a084c288745a489b09fcf848db8
rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-01-17 15:45:41 -0200

    update

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,20 +1,20 @@
 module Visible
   extend ActiveSupport::Concern
 
-  VALID_STATUSES = ['public', 'private', 'archived']
+  VALID_STATUSES = ['Public' , 'Private', 'Archived']
 
-  included do
-    validates :status, inclusion: { in: VALID_STATUSES }
-  end
+  # included do
+  #   validates :status, inclusion: { in: VALID_STATUSES }
+  # end
 
   class_methods do
     def public_count
-      where(status: 'public').count
+      where(status: 'Public').count
     end
   end
 
   def archived?
-    status == 'archived'
+    status == 'Archived'
   end
 end
   
